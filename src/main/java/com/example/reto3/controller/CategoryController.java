71392781ac0f894fb2ebf8174813c339bf0efd9a
luiscristomt <luis.cristo.mt@usa.edu.co>
2022-10-10 15:43:26 -0500

    correjir error en CategoryController

--- src/main/java/com/example/reto3/controller/CategoryController.java
+++ src/main/java/com/example/reto3/controller/CategoryController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/Controller")
+@RequestMapping("/api/Category")
 public class CategoryController {
     @Autowired
     private CategoryService categoryService;
